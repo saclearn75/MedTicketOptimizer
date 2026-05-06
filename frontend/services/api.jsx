7376c236f92599b3281ab799741ff224e32e3f3f
saclearn75 <saclearn75@gmail.com>
2026-05-05 22:27:15 -0700

    full v1 demo

--- frontend/services/api.jsx
+++ frontend/services/api.jsx
@@ -1,16 +1,17 @@
 // frontend/src/services/api.js
 export async function analyzeTicket(ticketText) {
-  const response = await fetch("http://localhost:5173/analyze-ticket", {
+    console.log ('sending text :'+ticketText)
+  const response = await fetch("http://localhost:8000/ticket", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ ticket_text: ticketText }),
+    body: JSON.stringify({ "text": ticketText }),
   });
 
   if (!response.ok) {
     throw new Error("Failed to analyze ticket");
   }
-
+//   console.log ('received response: '+ response.json())
   return response.json();
 }
